3fbb8fb41411ace7ea4f9ef9eeced75d9494c20a
iarpittomar <arpittomar7740@gmail.com>
2021-06-05 19:14:05 +0530

    feat: implement patch request

--- app.js
+++ app.js
@@ -68,6 +68,23 @@ app.post("/api/v1/tours", (req, res) => {
   );
 });
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      message: "SUccess",
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
